e04132d6d5e655f5536f166f4c7f3cffc782f8fd
GO3LIN <benmoha.mehdi@gmail.com>
2022-11-24 22:48:08 +0100

    first commit

--- index.ts
+++ index.ts
@@ -1,9 +1,41 @@
-import './style.css';
+import * as Rx from 'rxjs';
 
-import { of, map, Observable } from 'rxjs';
+/*
+// pending
+const promesse = new Promise((resolve, reject) => {
+  console.log('ok');
+  // fullfilled
+  resolve('o');
+  // rejected
+  reject('y');
+});
+promesse.then((c) => console.log('ok2'));
+*/
+const wakeUp$ = Rx.of('woke up');
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
+const goToWork = Rx.of('goToWork').pipe(
+  Rx.delay(500),
+  Rx.finalize(() => console.log('work finilazed'))
+);
+const openStore = Rx.of('openStore').pipe(
+  Rx.delay(10),
+  Rx.finalize(() => console.log('store finilazed'))
+);
+const driveLambo = Rx.of('driveLambo');
+const getMarried = Rx.of('getMarried');
+const goToMiami = Rx.of('goToMiami');
+
+const sub1 = wakeUp$
+  .pipe(
+    Rx.mergeMap((c) => {
+      console.log(c);
+      return goToWork;
+    }),
+    Rx.mergeMap((c) => {
+      console.log('went to work');
+      return openStore;
+    })
+  )
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
